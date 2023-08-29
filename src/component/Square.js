77a71959e19df70045de8fe3b841947ba0a436b6
vaishnavi1902 <vaishnavikumbhakarna467@gmail.com>
2023-08-29 16:51:25 +0530

    tic-tac-toe using reactJS

---
+++ src/component/Square.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Square(props) {
+  return (
+        <button className={props.value?'btn disabled' : 'btn'} 
+        onClick={props.onClick}>
+            {props.value}
+        </button>
+  )
+}
